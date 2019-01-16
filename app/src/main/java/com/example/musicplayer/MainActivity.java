4bf3181868392e6d84b64b9de93957e46dde1152
Feral <91yjw@naver.com>
2019-01-17 00:54:57 +0900

    add controller. got actionbar problem.

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -18,6 +18,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.MediaController;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -25,13 +26,15 @@ import java.util.Comparator;
 
 import com.example.musicplayer.MusicService.MusicBinder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MediaController.MediaPlayerControl {
     private final int MY_PERMISSIONS_REQUEST_READ_EXT_STORAGE = 100;
     private ArrayList<Song> songList;
     private ListView songView;
     private MusicService musicSrv;
     private Intent playIntent;
     private boolean musicBound = false;
+    private MusicController controller;
+    private boolean paused = false, playbackPaused = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +52,39 @@ public class MainActivity extends AppCompatActivity {
                 return a.getTitle().compareTo(b.getTitle());
             }
         });
+
+        setController();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(playIntent == null){
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        paused = true;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(paused){
+            setController();
+            paused = false;
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        controller.hide();
+        super.onStop();
     }
 
     // connect to the service.
@@ -69,19 +105,14 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-        if(playIntent == null){
-            playIntent = new Intent(this, MusicService.class);
-            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
-            startService(playIntent);
-        }
-    }
-
     public void songPicked(View view){
         musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
         musicSrv.playSong();
+        if(playbackPaused){
+            setController();
+            playbackPaused = false;
+        }
+        controller.show(0);
     }
 
     @Override
@@ -89,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         //menu item selected
         switch(item.getItemId()) {
             case R.id.action_shuffle:
-                // shuffle.
+                musicSrv.setShuffle();
                 break;
             case R.id.action_end:
                 stopService(playIntent);
@@ -183,5 +214,108 @@ public class MainActivity extends AppCompatActivity {
             // other 'case' lines to check for other
             // permissions this app might request
         }
+    }
+
+    private void setController(){
+        // et the controller up.
+        controller = new MusicController(this);
+
+        controller.setPrevNextListeners(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                playNext();
+            }
+        }, new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                playPrev();
+            }
+        });
+
+        controller.setMediaPlayer(this);
+        controller.setAnchorView(findViewById(R.id.song_list));
+        controller.setEnabled(true);
+    }
+
+    // play next.
+    private void playNext(){
+        musicSrv.playNext();
+        if(playbackPaused){
+            setController();
+            playbackPaused = false;
+        }
+        controller.show(0);
+    }
+
+    // play previous.
+    private void playPrev(){
+        musicSrv.playPrev();
+        if(playbackPaused){
+            setController();
+            playbackPaused = false;
+        }
+        controller.show(0);
+    }
+
+    @Override
+    public void start() {
+        musicSrv.go();
+    }
+
+    @Override
+    public void pause() {
+        playbackPaused = true;
+        musicSrv.pausePlayer();
+    }
+
+    @Override
+    public int getDuration() {
+        if(musicSrv != null && musicBound && musicSrv.isPng())
+            return musicSrv.getDur();
+        else return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        if(musicSrv != null && musicBound && musicSrv.isPng())
+            return musicSrv.getPosn();
+        else return 0;
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        musicSrv.seek(pos);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if(musicSrv != null && musicBound)
+            return musicSrv.isPng();
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
     }
 }
--- app/src/main/java/com/example/musicplayer/MusicService.java
+++ app/src/main/java/com/example/musicplayer/MusicService.java
@@ -1,5 +1,7 @@
 package com.example.musicplayer;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ContentUris;
 import android.content.Intent;
@@ -13,6 +15,7 @@ import android.provider.MediaStore;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MusicService extends Service implements
         MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
@@ -24,6 +27,10 @@ public class MusicService extends Service implements
     // current position
     private int songPosn;
     private final IBinder musicBind = new MusicBinder();
+    private String songTitle = "";
+    private static final int NOTIFY_ID = 1;
+    private boolean shuffle = false;
+    private Random rand;
 
     public void onCreate(){
         // create the service.
@@ -32,8 +39,14 @@ public class MusicService extends Service implements
         songPosn = 0;
         // create player.
         player = new MediaPlayer();
+        rand = new Random();
 
         initMusicPlayer();
+    }
+
+    @Override
+    public void onDestroy(){
+        stopForeground(true);
     }
 
     public void initMusicPlayer(){
@@ -60,6 +73,7 @@ public class MusicService extends Service implements
 
         // get song.
         Song playSong = songs.get(songPosn);
+        songTitle = playSong.getTitle();
         // get ID.
         long currSong = playSong.getID();
         // set uri.
@@ -93,10 +107,15 @@ public class MusicService extends Service implements
 
     @Override
     public void onCompletion(MediaPlayer mp) {
+        if(player.getCurrentPosition() > 0){
+            mp.reset();
+            playNext();
+        }
     }
 
     @Override
     public boolean onError(MediaPlayer mp, int what, int extra) {
+        mp.reset();
         return false;
     }
 
@@ -104,5 +123,74 @@ public class MusicService extends Service implements
     public void onPrepared(MediaPlayer mp) {
         // start playback.
         mp.start();
+
+        Intent notIntent = new Intent(this, MainActivity.class);
+        notIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendInt = PendingIntent.getActivity(this, 0,
+                notIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification.Builder builder = new Notification.Builder(this);
+
+        builder.setContentIntent(pendInt)
+                .setSmallIcon(R.drawable.play)
+                .setTicker(songTitle)
+                .setOngoing(true)
+                .setContentTitle("Playing")
+                .setContentText(songTitle);
+        Notification not = builder.build();
+
+        startForeground(NOTIFY_ID, not);
+    }
+
+    public int getPosn(){
+        return player.getCurrentPosition();
+    }
+
+    public int getDur(){
+        return player.getDuration();
+    }
+
+    public boolean isPng(){
+        return player.isPlaying();
+    }
+
+    public void pausePlayer(){
+        player.pause();
+    }
+
+    public void seek(int posn){
+        player.seekTo(posn);
+    }
+
+    public void go(){
+        player.start();
+    }
+
+    // skip to previous.
+    public void playPrev(){
+        songPosn--;
+        if(songPosn < 0) songPosn = songs.size() - 1;
+        playSong();
+    }
+
+    // skip to next.
+    // 랜덤 재생 시 queue를 이용해서 모든 노래가 재생되기 전에 동일한 노래가 반복 재생되는 것을 방지할 수 있다.
+    public void playNext(){
+        if(shuffle){
+            int newSong = songPosn;
+            while(newSong == songPosn){
+                newSong = rand.nextInt(songs.size());
+            }
+            songPosn = newSong;
+        } else {
+            songPosn++;
+            if(songPosn >= songs.size()) songPosn = 0;
+        }
+        playSong();
+    }
+
+    public void setShuffle(){
+        if(shuffle) shuffle = false;
+        else shuffle = true;
     }
 }
